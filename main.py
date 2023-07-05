e5eea6bd8e2b17d4237c93ed4d8143cdabab4d4f
Arnaldo Garat <arnaldogarat.thoughtworks@latam.com>
2023-07-05 18:25:55 -0400

    chore: lint

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
 import asyncio
-from shazamio import Shazam
-from pydub import AudioSegment
 import tempfile
+import time
+import sounddevice as sd
+from pydub import AudioSegment
+from shazamio import Shazam
+from scipy.io.wavfile import write
 
 RATE = 44100
 DURATION = 15
@@ -20,15 +21,25 @@ def convert_to_mp3(wav_file_path, mp3_file_path):
 async def recognize_mp3(mp3_file_path):
     shazam = Shazam()
     out = await shazam.recognize_song(mp3_file_path)
-    print(out)
+    return out
 
-
-if __name__ == '__main__':
+def recognize_song():
     temp_wav = tempfile.NamedTemporaryFile(suffix='.wav')
-    print(temp_wav.name)
     temp_mp3 = tempfile.NamedTemporaryFile(suffix='.mp3')
-    print(temp_mp3.name)
     record_audio(temp_wav.name)
     convert_to_mp3(temp_wav.name, temp_mp3.name)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(recognize_mp3(temp_mp3.name))
+    return loop.run_until_complete(recognize_mp3(temp_mp3.name))
+
+if __name__ == '__main__':
+    while True:
+        recognized_song = recognize_song()
+        if not recognized_song['matches']:
+            print('No song recognized, waiting 5 seconds...')
+            time.sleep(5)
+        else:
+            print('Song recognized!')
+            print("Artist: ", recognized_song['track']['subtitle'])
+            print("Artist: ", recognized_song['track']['title'])
+            print('Waiting 30 seconds for next song...')
+            time.sleep(5)
